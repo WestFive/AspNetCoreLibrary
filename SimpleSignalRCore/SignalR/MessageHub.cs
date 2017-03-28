d40992b88d850c5dd36e875e6c55eabf5e3926a3
westfive <chenlong5469@163.com>
2017-03-28 15:11:31 +0800

    SimpleSignalRCore

---
+++ SimpleSignalRCore/SignalR/MessageHub.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using Microsoft.AspNetCore.SignalR.Hubs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SimpleSignalRCore.SignalR
+{
+
+    public class MessageHub:Hub
+    {
+        public void SendMessage(string Message)
+        {
+            Clients.All.GetMessage(Message);
+        }
+
+        public override Task OnConnected()
+        {
+            Clients.All.GetMessage(Context.ConnectionId + "connect...");
+
+            return base.OnConnected();
+        }
+
+        public override Task OnDisconnected(bool stopCalled)
+        {
+
+            Clients.All.GetMessage(Context.ConnectionId + "DisConnect");
+            return base.OnDisconnected(stopCalled);
+        }
+    }
+}
